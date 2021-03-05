9d3e52a09b0dff2d89d9d3d60128992aacdbfa6f
henrylong <henrylong719@gmail.com>
2021-03-06 10:07:33 +1030

    before refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,75 @@
 class ArticlesController < ApplicationController
 
-  def show
 
+  # it will run set_article before the listing actions 
+
+  def show
 
     # byebug
 
     @article = Article.find(params[:id])
 
+  end
+
+  def index
+
+    @articles = Article.all
+
+  end
+ 
+
+  def new
+
+    @article = Article.new
+
+  end
+
+  def edit 
+
+    @article = Article.find(params[:id])
+
+  end
+
+  def create
+    # render plain data in the page
+    # {"title"=>"harry porter", "description"=>"this is harry porter"}
+    # render plain: params[:article]
+
+    #(whitelist) require the top level key of article and permit title and description from there to be used 
+    # to create article instance
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+
+    if @article.save
+    
+      flash[:notice] = "Article was created successfully."
+
+      # ruby will extract id from @article object and use it to form the path
+      # redirect_to article_path(@article) or
+      redirect_to @article
+    else
+      render 'new'
+
+    end
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    # redirect to the article listing page
+    redirect_to articles_path
   end
 
 end
