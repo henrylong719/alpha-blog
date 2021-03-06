f795f271772c5dc0bbde3d20c1145deb88713c93
henrylong <henrylong719@gmail.com>
2021-03-06 21:07:48 +1030

    user login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
+  before_save {self.username = username.downcase}
+  
   has_many :articles
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }, 
@@ -10,4 +13,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }, 
                     length: {maximum: 105},
                     format: {with: VALID_EMAIL_REGEX}
+  
+  `has_secure_password`
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # show show routes and index routes
   resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
